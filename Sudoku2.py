1896a9862f833d768949f0429d250adfae50cbf3
Xinrui Zhou <xinrui.zhou@genomicarebio.com>
2019-02-28 13:25:33 +0800

    simp

--- Sudoku2.py
+++ Sudoku2.py
@@ -1,40 +1,24 @@
-from tkinter import *
-from datetime import datetime
 import copy
+from datetime import datetime
+
 def init():
     global A,S
-    '''
-    SUDOKU=('006380000',#H2
-            '080009000',
-            '300200000',
-            '090008500',
-            '002003001',
-            '630040009',
-            '000621000',
-            '000090080',
-            '910004000')
-    
-    SUDOKU=('050000030',#H3
-            '000610000',
-            '006007000',
-            '031000000',
-            '000890002',
-            '072300850',
-            '007185000',
-            '060070500',
-            '000006020')
-    SUDOKU=('300460000',#H
-            '050000000',
-            '000090085',
-            '100000000',
-            '000000748',
-            '000003150',
-            '000078900',
-            '060310007',
-            '709600000')            
-            '''
+    #SUDOKU=('300460000','050000000','000090085','100000000','000000748','000003150','000078900','060310007','709600000')
+    #SUDOKU=('006380000','080009000','300200000','090008500','002003001','630040009','000621000','000090080','910004000')
+    #SUDOKU=('050000030','000610000','006007000','031000000','000890002','072300850','007185000','060070500','000006020')
+    #SUDOKU=('000040008','000900600','002000030','004001000','030080020','000030074','927000060','068320040','000600009')
+    #SUDOKU=('800000000','003600000','070090200','050007000','000045700','000100030','001000068','008500010','090000400')
     SUDOKU=('800000000','003600000','070090200','050007000','000045700','000100030','001000068','008500010','090000400')
-
+    SUDOKU=('000001000','904000000','300000689','096020407','037509218','000030950','712300004','000000305','000400000')
+    SUDOKU=('206000500','107000680','000000073','015082340','000009008','000004100','509068030','703900050','081540700')
+    
+    '''
+    SUDOKU = ()
+    with open('SUDOKU.in','r') as f:
+        infile = f.readlines()
+        for i in infile:
+            SUDOKU = SUDOKU + (i.strip(),)
+    '''
     A=[[]for x in range(9)]
     for i in range(len(SUDOKU)):
         A[i]=list(SUDOKU[i])
@@ -50,57 +34,47 @@ def init():
             for kk in range(int(ii/3)*3,int(ii/3)*3+3):
                 for ll in range(int(jj/3)*3,int(jj/3)*3+3):
                     S[kk][ll][A[ii][jj]]=0
-    draw_sudoku()
-
-def draw_sudoku():
-    global root
-    root.destroy()
-    root=Frame(master)
-    root.grid()
-    for i in range(9):
-        for j in range(9):
-            Label(root,text='  ',font=('Courier',30)).grid(row=(i+1)*2,column=(j+1)*2,sticky=E+W)
-            if A[i][j]!=0:
-                Label(root,text=str(A[i][j]),font=('Courier',15)).grid(row=(i+1)*2,column=(j+1)*2,sticky=E+W)
-            else:
-                tmp=''
-                for k in range(1,10):
-                    if S[i][j][k]==1:
-                        if len(tmp)==5:
-                            tmp=tmp+"\n"
-                        tmp=tmp+str(k)
-                Label(root,text=tmp,font=('Courier',8),bg='#999999').grid(row=(i+1)*2,column=(j+1)*2,sticky=E+W)
-    for i in range(2,19,2):
-        Label(root,text='-----').grid(row=7,column=i,sticky=E+W)
-        Label(root,text='-----').grid(row=13,column=i,sticky=E+W)
-        Label(root,text='|').grid(row=i,column=7,sticky=E+W)
-        Label(root,text='|').grid(row=i,column=13,sticky=E+W)
-    Button(root,text='RESET',command=init).grid(row=0,column=0,sticky=E+W)
-    Button(root,text='BASIC',command=runbasic).grid(row=6,column=0,sticky=E+W)
-    Button(root,text='BS',command=bs).grid(row=4,column=0,sticky=E+W)
-
 
 def testvalid():
     f=1
     for i in range(9):
         for j in range(9):
             if (A[i][j]==0)and(sum(S[i][j])==0):
-                f=0
-    if f==1:
-        return True
-    else:
-        return False
+                return False
+    return True
+
 
 def bs():
     global A
     global S
     global pointer
     global ijk
-    if testvalid():
-        nextcell()
-    else:
-        A=copy.deepcopy(trace[pointer-1])
-        S=copy.deepcopy(traSe[pointer-1])
+    global endflag
+    if pointer < 1:
+        print('END')
+        
+        endflag=1
+        return
+    S=copy.deepcopy(traSe[pointer-1])
+    A=list(map(list,trace[pointer-1]))
+
+    for k in range(ijk[pointer-1][2]+1,10):
+        if S[ijk[pointer-1][0]][ijk[pointer-1][1]][k]==1:
+            A[ijk[pointer-1][0]][ijk[pointer-1][1]]=k
+            ijk[pointer-1][2]=k
+            S[ijk[pointer-1][0]][ijk[pointer-1][1]][A[ijk[pointer-1][0]][ijk[pointer-1][1]]]=0
+            for kk in range(9):
+                S[ijk[pointer-1][0]][kk][A[ijk[pointer-1][0]][ijk[pointer-1][1]]]=0
+                S[kk][ijk[pointer-1][1]][A[ijk[pointer-1][0]][ijk[pointer-1][1]]]=0
+            for kk in range(int(ijk[pointer-1][0]/3)*3,int(ijk[pointer-1][0]/3)*3+3):
+                for ll in range(int(ijk[pointer-1][1]/3)*3,int(ijk[pointer-1][1]/3)*3+3):
+                    S[kk][ll][A[ijk[pointer-1][0]][ijk[pointer-1][1]]]=0
+            return
+
+    pointer = pointer-1
+    S=copy.deepcopy(traSe[pointer-1])
+    A=list(map(list,trace[pointer-1]))
+    while True:
         for k in range(ijk[pointer-1][2]+1,10):
             if S[ijk[pointer-1][0]][ijk[pointer-1][1]][k]==1:
                 A[ijk[pointer-1][0]][ijk[pointer-1][1]]=k
@@ -112,33 +86,20 @@ def bs():
                 for kk in range(int(ijk[pointer-1][0]/3)*3,int(ijk[pointer-1][0]/3)*3+3):
                     for ll in range(int(ijk[pointer-1][1]/3)*3,int(ijk[pointer-1][1]/3)*3+3):
                         S[kk][ll][A[ijk[pointer-1][0]][ijk[pointer-1][1]]]=0
-                draw_sudoku()
                 return
         pointer = pointer-1
-        A=copy.deepcopy(trace[pointer-1])
+        if pointer < 1:
+            print('END')
+            endflag=1
+            return
         S=copy.deepcopy(traSe[pointer-1])
-        while True:
-            for k in range(ijk[pointer-1][2]+1,10):
-                if S[ijk[pointer-1][0]][ijk[pointer-1][1]][k]==1:
-                    A[ijk[pointer-1][0]][ijk[pointer-1][1]]=k
-                    ijk[pointer-1][2]=k
-                    S[ijk[pointer-1][0]][ijk[pointer-1][1]][A[ijk[pointer-1][0]][ijk[pointer-1][1]]]=0
-                    for kk in range(9):
-                        S[ijk[pointer-1][0]][kk][A[ijk[pointer-1][0]][ijk[pointer-1][1]]]=0
-                        S[kk][ijk[pointer-1][1]][A[ijk[pointer-1][0]][ijk[pointer-1][1]]]=0
-                    for kk in range(int(ijk[pointer-1][0]/3)*3,int(ijk[pointer-1][0]/3)*3+3):
-                        for ll in range(int(ijk[pointer-1][1]/3)*3,int(ijk[pointer-1][1]/3)*3+3):
-                            S[kk][ll][A[ijk[pointer-1][0]][ijk[pointer-1][1]]]=0
-                    draw_sudoku()
-                    return
-            pointer = pointer-1
-            A=copy.deepcopy(trace[pointer-1])
-            S=copy.deepcopy(traSe[pointer-1])
-
+        A=list(map(list,trace[pointer-1]))
 
 def nextcell():
     global pointer
     global endflag
+    global A
+    global S
     for i in range(9):
         for j in range(9):
             if A[i][j]==0:
@@ -147,7 +108,7 @@ def nextcell():
                         trace.append([])
                         ijk.append([])
                         traSe.append([])
-                        trace[pointer]=copy.deepcopy(A)
+                        trace[pointer]=list(map(list,A))
                         ijk[pointer]=[i,j,k]
                         traSe[pointer]=copy.deepcopy(S)
                         pointer += 1
@@ -159,18 +120,24 @@ def nextcell():
                         for kk in range(int(i/3)*3,int(i/3)*3+3):
                             for ll in range(int(j/3)*3,int(j/3)*3+3):
                                 S[kk][ll][A[i][j]]=0
-                        draw_sudoku()
                         return
-    endflag=1
+    global count
+    '''
+    
+    if count > 1:
+        endflag = 1
+        print('MULTIPLE SOLUTIONS.\n')
+    else:
 
-def runbasic():
-    COMP=''
-    while COMP != str(A):
-        COMP=str(A)
-        basic()
+    '''
+    count += 1
+    print('No. ',count,' Time elapse : ',datetime.now()-start)
+    for i in range(9):
+        print(str(A[i])[1:-1])
+    print()
+    bs()
 
 def basic():
-
     for i in range(9):#single value in 1-cell
         for j in range(9):
             if sum(S[i][j])==1:
@@ -215,6 +182,7 @@ def basic():
                 for kk in range(int(t/3)*3,int(t/3)*3+3):
                     for ll in range(int(j/3)*3,int(j/3)*3+3):
                         S[kk][ll][A[t][j]]=0
+        '''
         for m in range(9):#single value in 9-cell
             s=0
             for i in range(int(m/3)*3,int(m/3)*3+3):
@@ -232,17 +200,35 @@ def basic():
                 for kk in range(int(ti/3)*3,int(ti/3)*3+3):
                     for ll in range(int(tj/3)*3,int(tj/3)*3+3):
                         S[kk][ll][A[ti][tj]]=0
+        '''
 
-    draw_sudoku()
-master=Tk()
-root=Frame(master)
-root.grid()
-init()
-trace=[]
-traSe=[]
-ijk=[]
-pointer = 0
-endflag=0
+def Sudoku2_main():
+    global trace
+    global traSe
+    global ijk
+    global pointer
+    global endflag
+    global COMP
+    global count
+    global start
+    init()
+    trace=[]
+    traSe=[]
+    ijk=[]
+    pointer = 0
+    endflag=0
+    COMP=''
+    count = 0
+    start=datetime.now()
+    while endflag==0:
+        while COMP != str(A):
+            COMP=str(A)
+            basic()
+        if testvalid():
+            nextcell()
+        else:
+            bs()
+    print('Total time elapse : ',datetime.now()-start)
 
-master.title('SUDOKU (C) By AlanZhou 2017')
-master.mainloop()
+if __name__ == '__main__':
+    Sudoku2_main()
--- SudokuGUI.py
+++ SudokuGUI.py
@@ -1,14 +1,40 @@
-import copy
+from tkinter import *
 from datetime import datetime
-
+import copy
 def init():
     global A,S
-    #SUDOKU=('300460000','050000000','000090085','100000000','000000748','000003150','000078900','060310007','709600000')
-    #SUDOKU=('006380000','080009000','300200000','090008500','002003001','630040009','000621000','000090080','910004000')
-    #SUDOKU=('050000030','000610000','006007000','031000000','000890002','072300850','007185000','060070500','000006020')
-    #SUDOKU=('000040008','000900600','002000030','004001000','030080020','000030074','927000060','068320040','000600009')
-    SUDOKU=('800000000','003600000','070090200','050007000','000045700','000100030','001000068','008500010','090000400')
-    SUDOKU=('800000000','003600000','070090200','050007000','000045700','000100030','001000068','008500010','090000400')
+    '''
+    SUDOKU=('006380000',#H2
+            '080009000',
+            '300200000',
+            '090008500',
+            '002003001',
+            '630040009',
+            '000621000',
+            '000090080',
+            '910004000')
+    
+    SUDOKU=('050000030',#H3
+            '000610000',
+            '006007000',
+            '031000000',
+            '000890002',
+            '072300850',
+            '007185000',
+            '060070500',
+            '000006020')
+    SUDOKU=('300460000',#H
+            '050000000',
+            '000090085',
+            '100000000',
+            '000000748',
+            '000003150',
+            '000078900',
+            '060310007',
+            '709600000')            
+            '''
+    SUDOKU=('800000000','003600000','070090200','050007000','000045700','000100030','001000068','008500010','090000300')
+
     A=[[]for x in range(9)]
     for i in range(len(SUDOKU)):
         A[i]=list(SUDOKU[i])
@@ -24,6 +50,35 @@ def init():
             for kk in range(int(ii/3)*3,int(ii/3)*3+3):
                 for ll in range(int(jj/3)*3,int(jj/3)*3+3):
                     S[kk][ll][A[ii][jj]]=0
+    draw_sudoku()
+
+def draw_sudoku():
+    global root
+    root.destroy()
+    root=Frame(master)
+    root.grid()
+    for i in range(9):
+        for j in range(9):
+            Label(root,text='  ',font=('Courier',30)).grid(row=(i+1)*2,column=(j+1)*2,sticky=E+W)
+            if A[i][j]!=0:
+                Label(root,text=str(A[i][j]),font=('Courier',15)).grid(row=(i+1)*2,column=(j+1)*2,sticky=E+W)
+            else:
+                tmp=''
+                for k in range(1,10):
+                    if S[i][j][k]==1:
+                        if len(tmp)==5:
+                            tmp=tmp+"\n"
+                        tmp=tmp+str(k)
+                Label(root,text=tmp,font=('Courier',8),bg='#999999').grid(row=(i+1)*2,column=(j+1)*2,sticky=E+W)
+    for i in range(2,19,2):
+        Label(root,text='-----').grid(row=7,column=i,sticky=E+W)
+        Label(root,text='-----').grid(row=13,column=i,sticky=E+W)
+        Label(root,text='|').grid(row=i,column=7,sticky=E+W)
+        Label(root,text='|').grid(row=i,column=13,sticky=E+W)
+    Button(root,text='RESET',command=init).grid(row=0,column=0,sticky=E+W)
+    Button(root,text='BASIC',command=runbasic).grid(row=6,column=0,sticky=E+W)
+    Button(root,text='BS',command=bs).grid(row=4,column=0,sticky=E+W)
+
 
 def testvalid():
     f=1
@@ -44,10 +99,8 @@ def bs():
     if testvalid():
         nextcell()
     else:
-
+        A=copy.deepcopy(trace[pointer-1])
         S=copy.deepcopy(traSe[pointer-1])
-        A=list(map(list,trace[pointer-1]))
-
         for k in range(ijk[pointer-1][2]+1,10):
             if S[ijk[pointer-1][0]][ijk[pointer-1][1]][k]==1:
                 A[ijk[pointer-1][0]][ijk[pointer-1][1]]=k
@@ -59,13 +112,11 @@ def bs():
                 for kk in range(int(ijk[pointer-1][0]/3)*3,int(ijk[pointer-1][0]/3)*3+3):
                     for ll in range(int(ijk[pointer-1][1]/3)*3,int(ijk[pointer-1][1]/3)*3+3):
                         S[kk][ll][A[ijk[pointer-1][0]][ijk[pointer-1][1]]]=0
+                draw_sudoku()
                 return
         pointer = pointer-1
-
+        A=copy.deepcopy(trace[pointer-1])
         S=copy.deepcopy(traSe[pointer-1])
-        A=list(map(list,trace[pointer-1]))
-
-
         while True:
             for k in range(ijk[pointer-1][2]+1,10):
                 if S[ijk[pointer-1][0]][ijk[pointer-1][1]][k]==1:
@@ -78,16 +129,12 @@ def bs():
                     for kk in range(int(ijk[pointer-1][0]/3)*3,int(ijk[pointer-1][0]/3)*3+3):
                         for ll in range(int(ijk[pointer-1][1]/3)*3,int(ijk[pointer-1][1]/3)*3+3):
                             S[kk][ll][A[ijk[pointer-1][0]][ijk[pointer-1][1]]]=0
+                    draw_sudoku()
                     return
             pointer = pointer-1
-            if pointer < 1:
-                print('WRONG SUDOKU')
-                global endflag
-                endflag=1
-                return
-
+            A=copy.deepcopy(trace[pointer-1])
             S=copy.deepcopy(traSe[pointer-1])
-            A=list(map(list,trace[pointer-1]))
+
 
 def nextcell():
     global pointer
@@ -100,11 +147,9 @@ def nextcell():
                         trace.append([])
                         ijk.append([])
                         traSe.append([])
-
-                        trace[pointer]=list(map(list,A))
+                        trace[pointer]=copy.deepcopy(A)
                         ijk[pointer]=[i,j,k]
                         traSe[pointer]=copy.deepcopy(S)
-                        
                         pointer += 1
                         A[i][j]=k
                         S[i][j][A[i][j]]=0
@@ -114,11 +159,18 @@ def nextcell():
                         for kk in range(int(i/3)*3,int(i/3)*3+3):
                             for ll in range(int(j/3)*3,int(j/3)*3+3):
                                 S[kk][ll][A[i][j]]=0
+                        draw_sudoku()
                         return
     endflag=1
 
+def runbasic():
+    COMP=''
+    while COMP != str(A):
+        COMP=str(A)
+        basic()
+
 def basic():
-    
+
     for i in range(9):#single value in 1-cell
         for j in range(9):
             if sum(S[i][j])==1:
@@ -132,9 +184,7 @@ def basic():
                         for kk in range(int(i/3)*3,int(i/3)*3+3):
                             for ll in range(int(j/3)*3,int(j/3)*3+3):
                                 S[kk][ll][A[i][j]]=0
-    
     for k in range(1,10):
-        
         for i in range(9):#single value in row
             s=0
             for j in range(9):
@@ -150,7 +200,6 @@ def basic():
                 for kk in range(int(i/3)*3,int(i/3)*3+3):
                     for ll in range(int(t/3)*3,int(t/3)*3+3):
                         S[kk][ll][A[i][t]]=0
-        
         for j in range(9):#single value in column
             s=0
             for i in range(9):
@@ -166,7 +215,6 @@ def basic():
                 for kk in range(int(t/3)*3,int(t/3)*3+3):
                     for ll in range(int(j/3)*3,int(j/3)*3+3):
                         S[kk][ll][A[t][j]]=0
-        '''
         for m in range(9):#single value in 9-cell
             s=0
             for i in range(int(m/3)*3,int(m/3)*3+3):
@@ -184,21 +232,17 @@ def basic():
                 for kk in range(int(ti/3)*3,int(ti/3)*3+3):
                     for ll in range(int(tj/3)*3,int(tj/3)*3+3):
                         S[kk][ll][A[ti][tj]]=0
-        '''
+
+    draw_sudoku()
+master=Tk()
+root=Frame(master)
+root.grid()
 init()
 trace=[]
 traSe=[]
 ijk=[]
 pointer = 0
 endflag=0
-COMP=''
-start=datetime.now()
-while endflag==0:
-    while COMP != str(A):
-        COMP=str(A)
-        basic()
-    bs()
-print(datetime.now()-start)
-for i in range(9):
-    print(A[i])
-print()
+
+master.title('SUDOKU (C) By AlanZhou 2017')
+master.mainloop()
